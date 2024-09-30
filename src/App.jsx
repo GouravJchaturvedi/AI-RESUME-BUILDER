70fb59624fd4a960a57a5fd3f58f151016b177d7
Gourav Chaturvedi <chaturvedigourav47@gmail.com>
2024-09-30 19:11:39 +0530

    Header Design

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,19 @@ import "./App.css";
 import { Button } from "./components/ui/button";
 import { Navigate, Outlet } from "react-router-dom";
 import { useUser } from "@clerk/clerk-react";
+import Header from "./components/custom/Header";
 
 function App() {
-  const {user , isLoaded , isSignedIn} = useUser()
+  const { user, isLoaded, isSignedIn } = useUser();
   const [count, setCount] = useState(0);
 
-  if(!isSignedIn && isLoaded){
-    return <Navigate to="/auth/sign-in"/>
-  }
+  // if(!isSignedIn && isLoaded){
+  //   return <Navigate to="/auth/sign-in"/>
+  // }
 
   return (
     <>
+      <Header />
       <Outlet />
     </>
   );
